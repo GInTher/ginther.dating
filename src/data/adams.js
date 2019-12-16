6d198c47a8c541e5458d389c5587fcce695f3baa
Adam Ginther <adamginther@adams-macbook.lan>
2019-12-16 01:56:29 -0800

    More images

--- src/data/adams.js
+++ src/data/adams.js
@@ -1,23 +1,33 @@
 import engineer from "../images/occupations/engineer.jpg";
 import assistantRegionalManager from "../images/occupations/assistant-regional-manager.jpg";
+import bankRobber from "../images/occupations/bank-robber.jpg";
 import bartender from "../images/occupations/bartender.jpg";
 import basketballPlayer from "../images/occupations/basketball-player.jpg";
 // import beeKeeper from "../images/occupations/bee-keeper.jpg";
+import bodybuilder from "../images/occupations/bodybuilder.jpg";
+import boxer from "../images/occupations/boxer.jpg";
 import chef from "../images/occupations/chef.jpg";
 import collegeStudent from "../images/occupations/college-student.jpg";
 import cop from "../images/occupations/cop.jpg";
 import cowboy from "../images/occupations/cowboy.jpg";
+import daredevil from "../images/occupations/daredevil.jpg";
+import detective from "../images/occupations/detective.jpg";
 import dj from "../images/occupations/dj.jpg";
 import firefighter from "../images/occupations/fire-fighter.jpg";
 import lumberjack from "../images/occupations/lumberjack.jpg";
 import madScientist from "../images/occupations/mad-scientist.jpg";
 import mobBoss from "../images/occupations/mob-boss.jpg";
+import millionaire from "../images/occupations/millionaire.jpg";
+import newsAnchor from "../images/occupations/news-anchor.jpg";
 import ninja from "../images/occupations/ninja.jpg";
 import pokerPlayer from "../images/occupations/poker-player.jpg";
 import poleDancer from "../images/occupations/pole-dancer.jpg";
+import rapper from "../images/occupations/rapper.jpg";
 import rockstar from "../images/occupations/rockstar.jpg";
+import stayAtHomeFather from "../images/occupations/stay-at-home-father.jpg";
 import stayAtHomeSon from "../images/occupations/stay-at-home-son.jpg";
 import streetFighter from "../images/occupations/street-fighter.jpg";
+import yogaInstructor from "../images/occupations/yoga-instructor.jpg";
 
 export const Adams = [
   {
@@ -31,15 +41,35 @@ export const Adams = [
     occupation: "Assistant to the Regional Manager",
   },
   {
+    imageAlt: "Handsome man, with a big bag of money.",
+    imageSrc: bankRobber,
+    occupation: "Bank Robber",
+  },
+  {
     imageAlt: "Handsome man, serving liquor.",
     imageSrc: bartender,
     occupation: "Bartender",
+  },
+  {
+    imageAlt: "Handsome man, playing basketball.",
+    imageSrc: basketballPlayer,
+    occupation: "Basketball Player",
   },
   // {
   //   imageAlt: "Handsome man, keeping bees.",
   //   imageSrc: beeKeeper,
   //   occupation: "Bee Keeper",
   // },
+  {
+    imageAlt: "Handsome man, building a handsome body.",
+    imageSrc: bodybuilder,
+    occupation: "Body Builder",
+  },
+  {
+    imageAlt: "Handsome man, kicking ass.",
+    imageSrc: boxer,
+    occupation: "Boxer",
+  },
   {
     imageAlt: "Handsome man, serving a salad.",
     imageSrc: chef,
@@ -56,9 +86,14 @@ export const Adams = [
     occupation: "Cowboy",
   },
   {
-    imageAlt: "Handsome man, playing basketball.",
-    imageSrc: basketballPlayer,
-    occupation: "Basketball Player",
+    imageAlt: "Handsome man, doing a cool motorcycle trick.",
+    imageSrc: daredevil,
+    occupation: "Daredevil",
+  },
+  {
+    imageAlt: "Handsome man, being a great detective.",
+    imageSrc: detective,
+    occupation: "Detective",
   },
   {
     imageAlt: "Handsome man, being an amazing DJ.",
@@ -81,6 +116,11 @@ export const Adams = [
     occupation: "Mad Scientist",
   },
   {
+    imageAlt: "Handsome man, with a lot of money.",
+    imageSrc: millionaire,
+    occupation: "Millionaire",
+  },
+  {
     imageAlt: "A handsome mob boss.",
     imageSrc: mobBoss,
     occupation: "Mob Boss",
@@ -89,6 +129,11 @@ export const Adams = [
     imageAlt: "Handsome ninja, with a sword.",
     imageSrc: ninja,
     occupation: "Ninja",
+  },
+  {
+    imageAlt: "Handsome man, telling you the news.",
+    imageSrc: newsAnchor,
+    occupation: "News Anchor",
   },
   {
     imageAlt: "Handsome man, playing poker.",
@@ -106,9 +151,19 @@ export const Adams = [
     occupation: "Police Officer",
   },
   {
+    imageAlt: "Handsome rapper, with a beatbox.",
+    imageSrc: rapper,
+    occupation: "Rapper",
+  },
+  {
     imageAlt: "A handsome rockstar.",
     imageSrc: rockstar,
     occupation: "Rockstar",
+  },
+  {
+    imageAlt: "Handsome man, with his children.",
+    imageSrc: stayAtHomeFather,
+    occupation: "Stay-at-home Father",
   },
   {
     imageAlt: "Handsome man, eating chips.",
@@ -119,5 +174,10 @@ export const Adams = [
     imageAlt: "A handsome street fighter kicking some ass.",
     imageSrc: streetFighter,
     occupation: "Street Fighter",
+  },
+  {
+    imageAlt: "A handsome man teaching yoga.",
+    imageSrc: yogaInstructor,
+    occupation: "Yoga Instructor",
   },
 ];
