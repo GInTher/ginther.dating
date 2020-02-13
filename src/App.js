0c7e2fca01d19ca74006c67ca5e22a5414f0ed09
Adam Ginther <adam.ginther@metalabdesign.com>
2020-02-12 22:37:12 -0800

    Add debug info to analytics

--- src/App.js
+++ src/App.js
@@ -7,19 +7,17 @@ import Profile from "./pages/Profile";
 import SendMessage from "./pages/SendMessage";
 import { Adams } from "./data/adams";
 import { pink } from "@material-ui/core/colors";
-import {
-  Router,
-  Route,
-  Switch
-} from "react-router-dom";
-import ReactGA from 'react-ga';
-import createHistory from 'history/createBrowserHistory'
+import { Router, Route, Switch } from "react-router-dom";
+import ReactGA from "react-ga";
+import createHistory from "history/createBrowserHistory";
 
-const history = createHistory()
-ReactGA.initialize('UA-158087755-1');
-history.listen((location, action) => {
-    ReactGA.pageview(location.pathname + location.search);
-    console.log(location.pathname)
+const history = createHistory();
+ReactGA.initialize("UA-158087755-1", {
+  debug: true
+});
+history.listen(location => {
+  ReactGA.pageview(location.pathname + location.search);
+  console.log(location.pathname);
 });
 
 const theme = createMuiTheme({
@@ -34,19 +32,38 @@ function App() {
       <ThemeProvider theme={theme}>
         <Switch>
           <Route exact path="/" children={<Home />} />
-          <Route exact path="/swipe" render={(props) => <Swipe {...props} currentPage={"swipe"} />} />
-          <Route exact path="/messages" render={(props) => <Messages {...props} currentPage={"messages"} />} />
-          <Route exact path="/profile" render={(props) => <Profile {...props} currentPage={"profile"} />} />
+          <Route
+            exact
+            path="/swipe"
+            render={props => <Swipe {...props} currentPage={"swipe"} />}
+          />
+          <Route
+            exact
+            path="/messages"
+            render={props => <Messages {...props} currentPage={"messages"} />}
+          />
+          <Route
+            exact
+            path="/profile"
+            render={props => <Profile {...props} currentPage={"profile"} />}
+          />
           <Route exact path="/send-message" children={<SendMessage />} />
           {Adams.map((obj, index) => {
             return (
-                <Route
-                  path={`/messages/${obj.occupation
-                    .replace(/\s+/g, "-")
-                    .toLowerCase()}`}
-                  render={(props) => <SendMessage {...props} imageSrc={obj.imageSrc} imageAlt={obj.imageAlt} occupation={obj.occupation} />}
-                    key={index}
-                />
+              <Route
+                path={`/messages/${obj.occupation
+                  .replace(/\s+/g, "-")
+                  .toLowerCase()}`}
+                render={props => (
+                  <SendMessage
+                    {...props}
+                    imageSrc={obj.imageSrc}
+                    imageAlt={obj.imageAlt}
+                    occupation={obj.occupation}
+                  />
+                )}
+                key={index}
+              />
             );
           })}
         </Switch>
